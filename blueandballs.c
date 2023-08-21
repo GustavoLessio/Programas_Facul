d5d7f57d48c3becd5b7a89b63c35cb6cb809f126
GustavoLessio <130804444+GustavoLessio@users.noreply.github.com>
2023-08-21 19:34:08 -0300

    calculo perimetro d/um quadrado

---
+++ blueandballs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    double lado1, lado2, perimetro;
+
+    printf("Programa que calcula o perimetro de um quadradro\n\n");
+
+    printf("Quanto mede (em cm) o lado do quadrado?\n");
+    scanf("%lf", lado1);
+    scanf("%lf", lado2);
+
+    perimetro = 2*lado1 + 2*lado2;
+
+    printf("O perimetro do quadarado pede %2.lf cm", perimetro);
+    printf("\n\nObrigado por usar este progama\n")
+}
